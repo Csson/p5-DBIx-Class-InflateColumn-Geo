b51ec33faff516af51d41d0ddb857694ab1ba30b
Erik Carlsson <info@code301.com>
2018-10-27 11:45:18 +0000

    Init

---
+++ lib/DBIx/Class/InflateColumn/Geo.pm
@@ -0,0 +1,29 @@
+use 5.10.0;
+use strict;
+use warnings;
+
+package DBIx::Class::InflateColumn::Geo;
+
+# ABSTRACT: Short intro
+# AUTHORITY
+our $VERSION = '0.0100';
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use DBIx::Class::InflateColumn::Geo;
+
+=head1 DESCRIPTION
+
+DBIx::Class::InflateColumn::Geo is ...
+
+=head1 SEE ALSO
+
+=cut
